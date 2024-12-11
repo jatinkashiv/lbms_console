0fe2a807dabeeefb1b53592052c5728ef4d98547
Jatin Kashiv <jatinkashiv2003@gmail.com>
2024-12-11 13:51:00 +0530

    Added book Class & Changes in main class

--- book.java
+++ book.java
@@ -1,5 +1,55 @@
 package LBMS;
+import java.util.UUID;
 
-public class book {
+class book
+{
+    private String id;
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+    
+    book(String title,String author)
+    {
+        this.id = UUID.randomUUID().toString();
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getId()
+    {
+       return id; 
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getAuthor()
+    {
+       return author; 
+    }
+
+    public Boolean isBorrowedCheck()
+    {
+        return isBorrowed;
+    }
+
+    public void borrowBook()
+    {
+        isBorrowed = true;
+    }
+
+    public void returnBook()
+    {
+        isBorrowed = false;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return "BooK ID: " + id + ", Title: " + title + ", Author: " + author + ", Borrowed: " + isBorrowed;
+    }
     
 }
--- main.java
+++ main.java
@@ -3,6 +3,6 @@ package LBMS;
 public class main {
     public static void main(String args[])
     {
-        
+        System.out.print("LBMS is in Progress");
     }
 }
